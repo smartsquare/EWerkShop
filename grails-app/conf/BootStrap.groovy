6020e02e0ef53eada9eaa358d5a1649a19f071af
Alexander Niemann <niemann@smartsquare.de>
2012-05-18 12:47:47 +0200

    Introduced additional product w/o factsheet to test data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,19 +10,19 @@ class BootStrap {
     	
     	Product stichsaege = new Product(title: 'Stichi 6001', type: typeStichsaege, packagedWeightInGramm: 650)
     	stichsaege.save()
-    	Product kreissaege = new Product(title: 'Kreisi Mega-X', type: typeKreissaege, packagedWeightInGramm: 1890)
-    	kreissaege.save()
-    	kreissaege = new Product(title: 'Kreisi 3000', type: typeKreissaege, packagedWeightInGramm: 1430)
-        kreissaege.save()
+    	Product kreissaege1 = new Product(title: 'Kreisi 3000', type: typeKreissaege, packagedWeightInGramm: 1430)
+        kreissaege1.save()
+        Product kreissaege2 = new Product(title: 'Kreisi Mega-X', type: typeKreissaege, packagedWeightInGramm: 1890)
+        kreissaege2.save()
 
         ProductFactsheet.collection.getDB().dropDatabase()
 
     	ProductFactsheet factsStichsaege = new ProductFactsheet(productId: stichsaege.getId(), operatingVoltage: 220, powerInput: 400)
-    	factsStichsaege['bladeLength'] = 8
+    	factsStichsaege['dynBladeLength'] = 8
     	factsStichsaege.save()
 
-    	ProductFactsheet factsKreissaege = new ProductFactsheet(productId: kreissaege.getId(), operatingVoltage: 220, powerInput: 1200)
-    	factsKreissaege['bladeDiameter'] = 35
+    	ProductFactsheet factsKreissaege = new ProductFactsheet(productId: kreissaege2.getId(), operatingVoltage: 220, powerInput: 1200)
+    	factsKreissaege['dynBladeDiameter'] = 35
     	factsKreissaege.save()
     }
     def destroy = {
